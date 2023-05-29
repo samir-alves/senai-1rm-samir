324a0678dcd96ee621199bbabf8e3cd1e7bbd40c
Samir Alves Santana <37538174818@info.local>
2023-05-29 09:53:14 -0300

    v2

--- desafio23.py
+++ desafio23.py
@@ -1,6 +1,6 @@
 num1 = int(input("entre com o primeiro numero: "))
 num2 = int(input("entre com o segundo  numero: "))
-pergunta = (input ("entre com com a operacao desejada  a soma b mutiplicar c maior d menor  "))
+pergunta = (input ("entre com com a operacao desejada  1 soma 2 mutiplicar 3 maior 4 menor  "))
 def soma (num1 , num2):
     total = num1 + num2
     return total
@@ -14,7 +14,7 @@ def menor (num1, num2):
     total = min(num1 , num2)
     return total
 if pergunta == 1:
-   print(soma(num1,num2)) 
+   print(soma()) 
 if pergunta == 2:
    print(mult(num1,num2)) 
 if pergunta == 3:
